5fa212e109bdf630025a0ac0ca69dc5a1a7becba
Wilson Zhu <wzhu7148@gmail.com>
2017-10-17 09:22:06 -0500

    added reporter to display progess

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ observation = env.reset()
 
 # this is the fitness function
 def eval_genomes(genomes, config):
+  best_fitness = None
   for genome_id, genome in genomes:
     obs = env.reset()
     nnet =  neat.nn.FeedForwardNetwork.create(genome, config)
-    
+
     genome.fitness = 0
     # the total runtime of the environment
     for time_step in range(1600):
@@ -24,16 +25,25 @@ def eval_genomes(genomes, config):
       obs, reward, done, info = env.step(output)
       genome.fitness += reward
       if done:
+        if best_fitness is None:
+          best_fitness = genome.fitness
+        elif best_fitness < genome.fitness:
+          best_fitness = genome.fitness
         env.reset()
         break
+  print(best_fitness)
 
 def run(config_file):
   # load the configuration
   config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_file)
-  
-  # create the population
+
+  # create the population & display progress
+  # todo: implement reporter
   p = neat.Population(config)
-  
+  p.add_reporter(neat.StdOutReporter(True))
+  stats = neat.StatisticsReporter()
+  p.add_reporter(stats)
+
   winner = p.run(eval_genomes, 500)
   print(winner)
   
