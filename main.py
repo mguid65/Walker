18e98250ad3e3e20e9c908bfde8d8331b778e778
Wilson Zhu <wzhu7148@gmail.com>
2017-10-19 13:51:04 -0500

    made args optional so it doesnt break

--- main.py
+++ main.py
@@ -58,5 +58,7 @@ def run(config_file):
 if __name__ == '__main__':
   local_dir = os.path.dirname(__file__)
   config_path = os.path.join(local_dir, 'config')
-  render = sys.argv[1]
+  render = False
+  if len(sys.argv) == 2:
+    render = True
   run(config_path)
