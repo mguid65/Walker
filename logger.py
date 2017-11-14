9ec2b475faad7e7665b58e14f31b4e820f7bf5b4
Wilson <wzhu7148@gmail.com>
2017-11-14 17:20:43 -0600

    Update logger.py

--- logger.py
+++ logger.py
@@ -5,32 +5,30 @@ from neat.population import Population
 from neat.reporting import BaseReporter
 from neat.math_util import mean
 from neat.six_util import itervalues, iterkeys
-import csv 
+import csv
+
 class logger(BaseReporter):
   def __init__(self):
     self.generation = None
-
-     
     
   def start_generation(self, generation):
     self.generation = generation
   
   def end_generation(self, config, population, species_set):
     pass
-    
-  def post_evaluate(self, config, population, species, best_genome):
-    
+  
+  def post_evaluate(self, config, population, species, best_genome):  
     fitnesses = [c.fitness for c in itervalues(population)]
     fit_mean = mean(fitnesses)  
     species_ids = list(iterkeys(species.species))
     for i in species_ids:
       s = species.species[i]
-     
-
-      with open("data.csv", "a") as csv_file:
-        writer = csv.writer(csv_file, delimiter=',')
-        line=(str(self.generation), str(i), str(s.fitness), str(fit_mean))
-        writer.writerow(line)
+      self.log(self.generation, i, s.fitness, fit_mean)
+      
+      #with open("data.csv", "a") as csv_file:
+        #writer = csv.writer(csv_file, delimiter=',')
+        #line=(str(self.generation), str(i), str(s.fitness), str(fit_mean))
+        #writer.writerow(line)
       
   def complete_extinction(self):
     print('All species extinct.')
@@ -43,3 +41,9 @@ class logger(BaseReporter):
 
   def info(self, msg):
     pass
+  
+  def log(self, generation, id, fitness, fit_mean):
+    with open("data.csv", "a") as file:
+      log = csv.writer(file, delimiter=",")
+      data = (str(generation), str(id), str(fitness), str(fit_mean))
+      log.writerow(data)
